6fdb5b983570d02b4e467ae6c698826e93fb2d84
dadav <33197631+dadav@users.noreply.github.com>
2023-04-07 18:23:44 +0200

    foo

--- fetch.py
+++ fetch.py
@@ -2,11 +2,15 @@
 
 import sys
 import requests
-from icalendar import Calendar, Event, vRecur, vCalAddress, vDatetime
 from datetime import timedelta
+from icalendar import Calendar, Event, vRecur, vCalAddress, vDatetime
+
+CALENDAR_URL = "https://www.aalen.de/api/EventApiRules.php"
 
 
-CALENDAR_URL = 'https://www.aalen.de/api/EventApiRules.php'
+class ExitCodes:
+    OK: int = 0
+    ERR: int = 1
 
 
 def fetch_events_json() -> dict:
@@ -26,36 +30,42 @@ def json_to_ical(json_data: dict) -> Calendar:
     Returns a calendar.
     """
     cal = Calendar()
-    cal.add('prodid', '-//Aalen2ics//https://github.com/dadav/aalen2ics//')
-    cal.add('version', '2.0')
+    cal.add("prodid", "-//Aalen2ics//https://github.com/dadav/aalen2ics//")
+    cal.add("version", "2.0")
 
-    for jevent in sorted(json_data, key=lambda i: i['id']):
+    for jevent in sorted(json_data, key=lambda i: i["id"]):
         event = Event()
-        rule_data = jevent['rule'].split('\n')
+        rule_data = jevent["rule"].split("\n")
         if len(rule_data) != 2:
             continue
-        start, rule = [x.split(':', maxsplit=1)[1] for x in rule_data]
+        start, rule = [x.split(":", maxsplit=1)[1] for x in rule_data]
         rule = vRecur.from_ical(rule)
 
-        if 'title' in jevent:
-            event.add('summary', jevent['title'])
+        if "title" in jevent:
+            event.add("summary", jevent["title"])
 
-        event.add('dtstart', vDatetime.from_ical(start, timezone='Europe/Berlin'))
-        event.add('rrule', rule)
-        if 'url' in jevent:
-            event.add('url', jevent['url'])
-            event.add('description', jevent['url'])
+        event.add("dtstart", vDatetime.from_ical(start, timezone="Europe/Berlin"))
+        event.add("rrule", rule)
+        if "url" in jevent:
+            event.add("url", jevent["url"])
+            event.add("description", jevent["url"])
 
-        if 'location' in jevent:
-            event.add('location', jevent['location'])
+        if "location" in jevent:
+            event.add("location", jevent["location"])
 
-        if 'organiser' in jevent:
-            event.add('organizer', vCalAddress.from_ical(jevent['organiser']))
+        if "organiser" in jevent:
+            event.add("organizer", vCalAddress.from_ical(jevent["organiser"]))
 
-        if 'timeStart' in jevent and 'timeEnd' in jevent:
+        if "timeStart" in jevent and "timeEnd" in jevent:
             try:
-                duration = timedelta(hours=int(jevent['timeEnd'].split(':')[0]), minutes=int(jevent['timeEnd'].split(':')[1])) - timedelta(hours=int(jevent['timeStart'].split(':')[0]), minutes=int(jevent['timeStart'].split(':')[1]))
-                event.add('duration', duration)
+                duration = timedelta(
+                    hours=int(jevent["timeEnd"].split(":")[0]),
+                    minutes=int(jevent["timeEnd"].split(":")[1]),
+                ) - timedelta(
+                    hours=int(jevent["timeStart"].split(":")[0]),
+                    minutes=int(jevent["timeStart"].split(":")[1]),
+                )
+                event.add("duration", duration)
             except Exception:
                 pass
 
@@ -71,15 +81,15 @@ def main() -> int:
     returns 0 if everything went well and >0 if something failed.
     """
     out = sys.argv[1]
-    try:
-        events_json = fetch_events_json()
-        events_ics = json_to_ical(events_json)
-    except Exception:
-        return 1
-    with open(out, 'wb') as icsfile:
+
+    events_json = fetch_events_json()
+    events_ics = json_to_ical(events_json)
+
+    with open(out, "wb") as icsfile:
         icsfile.write(events_ics.to_ical())
-    return 0
+
+    return ExitCodes.OK
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main())
